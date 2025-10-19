6fc12f64404a7b197a3959543c43fe9381b13736
Bhargavvv412 <bhargavvv412@gmail.com>
2025-10-19 10:33:09 +0530

    lang serve done

---
+++ langserve_demo.py
@@ -0,0 +1,39 @@
+import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from langserve import add_routes
+import uvicorn
+
+load_dotenv()
+
+# Initialize Gemini with LangChain wrapper
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+
+parser = StrOutputParser()
+
+system_template = "Translate the following into {language}:"
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", system_template),
+    ("user", "{text}")
+])
+
+chain = prompt_template | llm | parser
+
+app= FastAPI(
+    title="simpleTranslator",
+    version="1.0",
+    description="A simple API server using Langchain's Runnable interfaces"
+)
+
+add_routes(
+    app,
+    chain,
+    path='/chain'
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="localhost",port=8000)
